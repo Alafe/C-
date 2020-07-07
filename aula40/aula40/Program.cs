ec98dd2254a373f8c3de27f3385148d56685d960
Alafe <alaferodrigo18@gmail,com>
2020-07-06 22:35:25 -0300

    aula 40 completa

---
+++ aula40/aula40/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace aula40
+{
+    class Program
+    {
+        //classe sealed
+        //não posso usala como classe base
+        //classe que não pode ser herdada
+        sealed class Veiculo
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
