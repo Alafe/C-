a607444b972f518d941c022827fa19e728eb2dab
Alafe <alaferodrigo18@gmail,com>
2020-07-06 21:24:36 -0300

    Terminando a aula36 e Fazendo a aula 37

--- aula36/aula36/Program.cs
+++ aula36/aula36/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+//membros protect
 namespace aula36
 {
     class Veiculo //classe Base
@@ -9,12 +9,33 @@ namespace aula36
         protected bool ligado;
 
 
-        public Veiculo(int velMax)
+        public Veiculo(int velMax) //contrutor
         {
             velAtual = 0;
             this.velMax = velMax;
             ligado = false;
         }
+
+        public bool getLigado()
+        {
+            return ligado;
+        }
+
+        public int getVelMax()
+        {
+            return velMax;
+        }
+    }
+
+    class Carro:Veiculo{ //derivada de veiculo
+        public string nome;
+
+        public Carro(string nome, int vm):base(vm)
+        {
+            this.nome = nome;
+            ligado = true;
+        }
+
     }
 
 
@@ -22,6 +43,13 @@ namespace aula36
     {
         static void Main(string[] args)
         {
+            Carro carro = new Carro("Bonitao", 120);
+
+            Console.WriteLine("nome:.........{0}",carro.nome);
+            Console.WriteLine("vel.Maxima:...{0}", carro.getVelMax());
+            Console.WriteLine("Ligado:.......{0}",carro.getLigado());
+
+            Console.ReadKey();
         }
     }
 }
---
+++ aula37/aula37/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace aula37
+{
+    //Herança/ordem de execução dos contrutores
+    class Base
+    {
+        public Base()
+        {
+            Console.WriteLine("Contrutor da Classe Base");
+        }
+    }
+
+    class Derivada1:Base
+    {
+        public Derivada1()
+        {
+            Console.WriteLine("Contrutor da Classe Derivada 1");
+        }
+    }
+
+    class Derivada2:Derivada1
+    {
+        public Derivada2()
+        {
+            Console.WriteLine("Contrutor da Classe Derivada 2");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Derivada2 derivada2 = new Derivada2();
+            Console.ReadKey();
+        }
+    }
+}
