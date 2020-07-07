3cd3cbaac5e9203ecae175cf27df2f43c23619c7
Alafe <alaferodrigo18@gmail,com>
2020-07-06 22:22:09 -0300

    aula 39 completa

---
+++ aula39/aula39/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace aula39
+{
+    class Program
+    {
+        //classe e métodos abstratos
+        abstract class Veiculo{ //classe base abstrata
+            protected int velMaxima;
+            protected int velAtual;
+            protected bool ligado;
+
+            public Veiculo()
+            {
+                ligado = false;
+                velAtual = 0;
+            }
+
+            public void setLigado(bool ligado)
+            {
+                this.ligado = ligado;
+            }
+
+            public int getVelAtual()
+            {
+                return velAtual;
+            }
+
+            abstract public void aceleracao(int mult);
+        }
+        
+        class Carro:Veiculo{ //classe derivada
+            public Carro()
+            {
+                velMaxima = 120;
+            }
+
+            override public void aceleracao(int mult)
+            {
+                velAtual += 10 * mult;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Carro carro1 = new Carro();
+
+            carro1.aceleracao(1);
+            carro1.aceleracao(-1);
+
+            Console.WriteLine(carro1.getVelAtual());
+            Console.ReadKey();
+        }
+    }
+}
